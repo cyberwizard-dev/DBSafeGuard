ad1da85abc8fbb7e4319a59e93f1daa6eadff070
Cyberwizard <53139650+cyberwizard-dev@users.noreply.github.com>
2023-11-20 15:12:33 +0100

    Message

--- packages/dbsafeguard/src/Message.php
+++ packages/dbsafeguard/src/Message.php
@@ -1,12 +1,33 @@
 <?php
 
 namespace Dbsafeguard;
-class Message
-{
-    public static function postInstall(): void
-    {
-        echo "This plugin was created by Cyberwizard \n\n Configuration\n";
-        echo "The package uses environment variables (DB_DATABASE, DB_USERNAME, DB_PASSWORD) to connect to the database. Ensure these variables are correctly set in your .env file.\n";
-    }
 
+use Illuminate\Console\Command;
+
+class Message extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dbsafeguard:post-install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Post-installation message for DBSafeguard package';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $this->info("This plugin was created by Cyberwizard \n\n Configuration");
+        $this->info('The package uses environment variables (DB_DATABASE, DB_USERNAME, DB_PASSWORD) to connect to the database. Ensure these variables are correctly set in your .env file.');
+    }
 }
